2a093c2db46ceab0625a22ce47e9e9103f1df022
MasonBishop <78995218+MasonBishop@users.noreply.github.com>
2021-07-18 01:03:55 -0500

    Add files via upload

---
+++ mod one assignment/app.js
@@ -0,0 +1,158 @@
+function problemOne(event) {
+
+  const target = event.target;
+  // code here:
+
+}
+document.getElementById("remove-me").addEventListener('click', problemOne);
+// reference the elment "#remove-me" and add an eventlistener for the click event:
+// your code here:
+var element=document.getElementById("#remove-me");
+
+
+element.remove();
+/* ---------------------------------------- */
+
+function problemTwo(event) {
+  const target = event.target;
+  // your code here:
+}
+
+// reference the elment "#remove-my-children" and add an eventlistener for the click event:
+// your code here
+var element=document.getElementById("#remove-my-children");
+element.remove();
+/* ---------------------------------------- */
+
+
+function problemThree(event) {
+
+  const target = event.target;
+  // your code here
+
+}
+
+// reference the elment "#remove-my-children-then-me" and add an eventlistener for the click event:
+// your code here:
+var element=document.getElementById("#remove-my-children-then-me");
+element.remove();
+/* ---------------------------------------- */
+
+function problemFour( event ) {
+
+  const target = event.target;
+  // your code here
+
+}
+
+// reference the elment "#add-children" and add an eventlistener for the click event:
+// your code here:
+var element=document.getElementById("#add-children");
+element.add();
+/* ---------------------------------------- */
+
+function problemFive({target}) {
+
+  const target = event.target;
+  // your code here
+
+}
+
+// reference the elment "#to-the-back" and add an eventlistener for the mouseup event:
+// your code here:
+var element=document.getElementById("#to-the-back");
+element.up();
+/* ---------------------------------------- */
+
+
+function problemSix({target}) {
+
+  const target = event.target;
+  // your code here
+
+}
+
+// reference the elment "#increment-child" and add an eventlistener for the click event:
+// your code here:
+var element=document.getElementById("#increment-child");
+element.increment();
+/* ---------------------------------------- */
+
+/* Maybe code goes here, if some scope is needed... */
+
+function problemSeven(event) {
+
+  const target = event.target;
+  // your code here
+
+  var element=document.getElementById("#increment-decrement");
+  element.decrement();
+}
+
+// reference the elment "#increment-decrement" and add an eventlistener for the click event:
+// your code here:
+
+/* ---------------------------------------- */
+
+
+function problemEight(event) {
+
+ const target = event.target;
+  // your code here
+
+}
+
+// reference the elment "#echo-user-input" and add an eventlistener for the keyup event:
+// your code here:
+var element=document.getElementById("#echo-user-input");
+element.keyup();
+/* ---------------------------------------- */
+
+/* Maybe code goes here, if some scope is needed... */
+
+function problemNine(event) {
+
+  event.preventDefault();
+  const target = event.target;
+  // your code here
+
+}
+
+// reference the elment "#paste-place" and add an eventlistener for the paste event:
+// your code here:
+var element=document.getElementById("#paste-place");
+element.paste();
+/* ---------------------------------------- */
+
+function problemTen(event) {
+  const target = event.target;
+  // your code here
+}
+
+// reference the elment "#show-hide" and add an eventlistener for the change event:
+// your code here:
+var element=document.getElementById("#show-hide");
+element.show();
+/* ---------------------------------------- */
+
+
+function problemEleven( event ) {
+
+  event.preventDefault();
+
+  const target = event.target;
+  // your code here
+
+}
+
+// your code here
+var element=document.getElementById("");
+element.();
+/* ---------------------------------------- */
+
+function problemTwelve ( ) {
+
+    // your code here
+}
+
+// your code here
