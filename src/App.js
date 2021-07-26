f69764421b3bc99e9fdb8700a50e764d13ad0d51
MasonBishop <78995218+MasonBishop@users.noreply.github.com>
2021-07-26 00:52:16 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-import TodoList from './TodoList'
+import Design from './Design'
 
 class App extends Component {
     render() {
         return (
 
             <div className="App">
-                <TodoList />
+                <Design/>
             </div>
         );
     }
 }
-    export default App;
+export default App;
+
