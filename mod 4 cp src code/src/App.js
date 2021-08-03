79ad62b1a92570f1a236bf3ccb668a626e90e34e
MasonBishop <78995218+MasonBishop@users.noreply.github.com>
2021-08-03 01:01:19 -0500

    Add files via upload

---
+++ mod 4 cp src code/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import Design from './Design'
+
+class App extends Component {
+    render() {
+        return (
+
+            <div className="App">
+                <Design/>
+            </div>
+        );
+    }
+}
+export default App;
+
